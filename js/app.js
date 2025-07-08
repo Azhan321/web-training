8c6da25c9791f6e69834b5dc22b653b6467d5d8a
Azhan321 <azhanarshad21@gmail.com>
2025-07-08 13:11:53 +0500

    calculator

--- js/app.js
+++ js/app.js
@@ -1,4 +1,40 @@
-function hello(){
-console.log("my name is azhan")
+const prompt = require('prompt-sync')();
+
+function calculator() {    
+  let num1 = parseFloat(prompt("Enter the first number:"));
+  let operator = prompt("Enter operator (+, -, *, /):");
+  let num2 = parseFloat(prompt("Enter the second number:"));
+  let result;
+
+  if (isNaN(num1) || isNaN(num2)) {
+    alert("Error: Invalid number input.");
+    return;
+  }
+
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      if (num2 === 0) {
+        alert("Error: Division by zero");
+        return;
+      }
+      result = num1 / num2;
+      break;
+    default:
+      alert("Error: Invalid operator");
+      return;
+  }
+
+  console.log("Result: " + result);
 }
-hello()
+
+calculator();
+
