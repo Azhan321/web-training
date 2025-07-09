88120591e25b8e4a1f6d72f47811cb5ed9c644d6
Azhan321 <azhanarshad21@gmail.com>
2025-07-09 11:59:19 +0500

    even odd array

--- js/array.js
+++ js/array.js
@@ -8,3 +8,17 @@ for (let i = 1; i < numbers.length; i++) {
 }
 
 console.log("Largest number is:", largest); // 100
+let numbers = [0,2, 7, 4, 9, 12, 5];
+let evenCount = 0;
+let oddCount = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    evenCount++;
+  }else{
+    oddCount++;
+  }
+}
+
+console.log("Even numbers count:", evenCount);
+console.log("Odd numbers count:", oddCount);
