42be38a5204a57e3148c019f6b7cc3fb58658e26
Azhan321 <azhanarshad21@gmail.com>
2025-07-09 13:16:10 +0500

    design simple page

--- js/array.js
+++ js/array.js
@@ -1,11 +1,11 @@
-let numbers = [10, 50, 25, 100, 60];
-let largest = numbers[0]; // assume first is largest
+// let numbers = [10, 50, 25, 100, 60];
+// let largest = numbers[0]; // assume first is largest
 
-for (let i = 1; i < numbers.length; i++) {
-  if (numbers[i] > largest) {
-    largest = numbers[i];
-  }
-}
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > largest) {
+//     largest = numbers[i];
+//   }
+// }
 
 console.log("Largest number is:", largest); // 100
 let numbers = [0,2, 7, 4, 9, 12, 5];
