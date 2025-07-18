db9eb21a5297bd8bb282c0c6b7cbf0d30db3b3ae
Azhan321 <azhanarshad21@gmail.com>
2025-07-18 12:50:48 +0500

    callback,promises and async/await func done

---
+++ async.js
@@ -0,0 +1,58 @@
+function sayBye() {
+  console.log("Goodbye!");
+}
+greetUser("Azhan", sayBye);
+function greetUser(name, callback) {
+  console.log("Hello " + name);
+  callback();
+}
+function fetchData(callback) {
+  setTimeout(() => {
+    console.log("Data fetched from server!");
+    callback();
+  }, 2000);
+}
+
+function displayData() {
+  console.log("Displaying data to the user...");
+}
+
+fetchData(displayData);
+let myPromise = new Promise((resolve, reject) => {
+  let success = true;
+
+  if (success) {
+    resolve("Data loaded successfully!");
+  } else {
+    reject("Error while loading data.");
+  }
+});
+
+myPromise
+  .then(result => {
+    console.log(result); // if success
+  })
+  .catch(error => {
+    console.log(error); // if error
+  }).finally( 
+    console.log("finaly")
+  );
+
+function getData() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("Data received!");
+    }, 2000);
+  });
+}
+
+async function showData() {
+  console.log("Fetching...");
+  const result = await getData();
+  console.log(result);
+}
+
+showData();
+
+
+
